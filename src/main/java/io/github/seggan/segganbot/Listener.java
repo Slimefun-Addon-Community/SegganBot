5b0ad3b1fbbcae8eabf3912162aaf5cd52bd237e
Seggan <segganew@gmail.com>
2021-01-14 21:48:33 -0500

    Fixed incorrect sf

--- src/main/java/io/github/seggan/segganbot/Listener.java
+++ src/main/java/io/github/seggan/segganbot/Listener.java
@@ -4,7 +4,12 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class Listener extends ListenerAdapter {
+    private static final Pattern INCORRECT_SLIMEFUN_PATTERN = Pattern.compile("(?<=[\\w\\d\\s])*[Ss]lime(?:F|( [Ff]))un(?=[\\w\\d\\s])*");
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent e) {
         if (e.getAuthor().isBot()) {
@@ -12,15 +17,21 @@ public final class Listener extends ListenerAdapter {
         }
 
         if (e.getMessage().getContentRaw().equals("?ping")) {
-            e.getChannel().sendMessage("Pong!").queue();
+            Util.sendMessage(e.getChannel(), "Pong!");
         }
 
         processIncorrectSlimefun(e);
     }
 
     private static void processIncorrectSlimefun(MessageReceivedEvent e) {
-        if (e.getMessage().getContentRaw().toLowerCase().contains("slime fun")) {
-            e.getChannel().sendMessage(Util.pingUser(e.getAuthor()) + " It's Slimefun, not \"slime fun\"").queue();
+        String msg = e.getMessage().getContentRaw();
+        Matcher matcher = INCORRECT_SLIMEFUN_PATTERN.matcher(msg);
+        while (matcher.find()) {
+            Util.sendMessage(e.getChannel(), String.format(
+                "%s It's Slimefun, not \"%s\"",
+                Util.pingUser(e.getAuthor()),
+                matcher.group()
+            ));
         }
     }
 }
--- src/main/java/io/github/seggan/segganbot/Util.java
+++ src/main/java/io/github/seggan/segganbot/Util.java
@@ -1,14 +1,20 @@
 package io.github.seggan.segganbot;
 
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 
 public final class Util {
-    private Util() {}
+    private Util() {
+    }
 
     public static String pingUser(User user) {
         return String.format(
             "<@%s>",
             user.getId()
         );
+    }
+
+    public static void sendMessage(MessageChannel channel, String message) {
+        channel.sendMessage(message).queue();
     }
 }
